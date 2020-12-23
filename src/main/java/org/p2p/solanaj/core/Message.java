db3f090959330cf3e04a39743e5daa9a5e5acaa3
b1acksun <tkachenko.igor88@gmail.com>
2020-12-23 13:21:09 +0200

    fix: typo

--- src/main/java/org/p2p/solanaj/core/Message.java
+++ src/main/java/org/p2p/solanaj/core/Message.java
@@ -34,7 +34,7 @@ public class Message {
         }
     }
 
-    private static final int RECENT_BLOCK_HASH_LENGT = 32;
+    private static final int RECENT_BLOCK_HASH_LENGTH = 32;
 
     private MessageHeader messageHeader;
     private String recentBlockhash;
@@ -101,7 +101,7 @@ public class Message {
 
         byte[] instructionsLength = ShortvecEncoding.encodeLength(compiledInstructions.size());
 
-        int bufferSize = MessageHeader.HEADER_LENGTH + RECENT_BLOCK_HASH_LENGT + accountAddressesLength.length
+        int bufferSize = MessageHeader.HEADER_LENGTH + RECENT_BLOCK_HASH_LENGTH + accountAddressesLength.length
                 + (accountKeysSize * PublicKey.PUBLIC_KEY_LENGTH) + instructionsLength.length
                 + compiledInstructionsLength;
 
