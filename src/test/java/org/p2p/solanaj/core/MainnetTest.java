960cac4c86d71743eeaea641eb9d5e60fbbadd21
skynetcapital <67430414+skynetcapital@users.noreply.github.com>
2020-12-29 11:35:33 -0800

    serum market struct poc

--- src/test/java/org/p2p/solanaj/core/MainnetTest.java
+++ src/test/java/org/p2p/solanaj/core/MainnetTest.java
@@ -6,6 +6,8 @@ import org.p2p.solanaj.rpc.RpcClient;
 import org.p2p.solanaj.rpc.RpcException;
 import org.p2p.solanaj.rpc.types.AccountInfo;
 
+import java.util.Arrays;
+import java.util.Base64;
 import java.util.List;
 import java.util.Map;
 
@@ -15,6 +17,14 @@ public class MainnetTest {
 
     private final RpcClient client = new RpcClient(Cluster.MAINNET);
     private final PublicKey publicKey = new PublicKey("skynetDj29GH6o6bAqoixCpDuYtWqi1rm8ZNx1hB3vq");
+
+    public static final int initialized = 1;  // Binary 00000001
+    public static final int market = 2;  // Binary 00000010
+    public static final int openOrders = 4;  // Binary 00000100
+    public static final int requestQueue = 8;  // Binary 00001000
+    public static final int eventQueue = 16;  // Binary 00010000
+    public static final int bids = 32;  // Binary 00100000
+    public static final int asks = 64;  // Binary 01000000
 
     @Test
     public void getAccountInfoBase64() {
@@ -61,6 +71,52 @@ public class MainnetTest {
 
             // Verify any balance
             assertTrue(balance > 0);
+        } catch (RpcException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void marketAccountTest() {
+        try {
+            // Pubkey of BTC/USDC market
+            final PublicKey publicKey = new PublicKey("CVfYa8RGXnuDBeGmniCcdkBwoLqVxh92xB1JqgRQx3F");
+
+
+            // Get account Info
+            final AccountInfo accountInfo = client.getApi().getAccountInfo(publicKey);
+            final List<String> accountData = accountInfo.getValue().getData();
+            final String base64Data = accountData.get(0);
+            System.out.println("Got data for pubkey CVfYa8RGXnuDBeGmniCcdkBwoLqVxh92xB1JqgRQx3F");
+            System.out.println("Raw data (Base64) = " + base64Data);
+
+            // assume every market is v2
+            // TODO handle the two v1 markets later
+
+            if (base64Data != null) {
+                //System.out.println("data=" + base64Data);
+                Base64.Decoder decoder = Base64.getDecoder();
+                byte[] bytes = decoder.decode(accountData.get(0));
+                System.out.println("Blob #1 (5 bytes) = " + new String(Arrays.copyOfRange(bytes, 0, 5)));
+
+                // Get account flags (next 8 bits)
+                byte accountFlags = Arrays.copyOfRange(bytes, 5, 6)[0];
+
+                System.out.println("Accounts Flags #1 (1 byte (up to 8 booleans))) = " + String.format("%8s", Integer.toBinaryString(accountFlags & 0xFF)).replace(' ', '0'));
+
+                boolean isMarketInitialized = false;
+                if ((accountFlags & initialized) == initialized)
+                {
+                    isMarketInitialized = true;
+                }
+
+
+                System.out.println("Bit #6 (Market initialized boolean) = " + isMarketInitialized);
+                System.out.println("Blob #2 (last 7 bytes) = " + new String(Arrays.copyOfRange(bytes, bytes.length - 7, bytes.length)));
+            }
+
+            // Verify any balance
+            assertTrue(true);
         } catch (RpcException e) {
             e.printStackTrace();
         }
