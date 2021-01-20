33dd02f9fb1aca7639fbbc55c311092c51e27445
skynetcapital <67430414+skynetcapital@users.noreply.github.com>
2021-01-20 09:15:27 -0800

    Add MemoProgram javadocs

--- src/main/java/org/p2p/solanaj/programs/MemoProgram.java
+++ src/main/java/org/p2p/solanaj/programs/MemoProgram.java
@@ -6,9 +6,19 @@ import org.p2p.solanaj.core.TransactionInstruction;
 import java.nio.charset.StandardCharsets;
 import java.util.Collections;
 
+/**
+ * Interface for the Memo program, used for writing UTF-8 data into Solana transactions.
+ */
 public class MemoProgram {
+    
     public static final PublicKey PROGRAM_ID = new PublicKey("Memo1UhkJRfHyvLMcVucJwxXeuD728EqVDDwQDxFMNo");
 
+    /**
+     * Returns a {@link TransactionInstruction} object containing instructions to call the Memo program with the
+     * specified memo.
+     * @param memo utf-8 string to be written into Solana transaction
+     * @return {@link TransactionInstruction} object with memo instruction
+     */
     public static TransactionInstruction writeUtf8(String memo) {
         final TransactionInstruction memoInstruction = new TransactionInstruction(
                 PROGRAM_ID,
