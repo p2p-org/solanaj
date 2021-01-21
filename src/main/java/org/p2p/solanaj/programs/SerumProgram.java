85ac495656ce4ca5c49c7b796c149525a5ca8e2f
skynetcapital <67430414+skynetcapital@users.noreply.github.com>
2021-01-21 15:18:12 -0800

    Add docs for SerumProgram keys

--- src/main/java/org/p2p/solanaj/programs/SerumProgram.java
+++ src/main/java/org/p2p/solanaj/programs/SerumProgram.java
@@ -1,10 +1,17 @@
 package org.p2p.solanaj.programs;
 
+import org.p2p.solanaj.core.Account;
+import org.p2p.solanaj.core.AccountMeta;
 import org.p2p.solanaj.core.TransactionInstruction;
+import org.p2p.solanaj.serum.Market;
+import org.p2p.solanaj.serum.Order;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SerumProgram extends Program {
 
-    public static TransactionInstruction placeOrder() {
+    public static TransactionInstruction placeOrder(Account account, Market market, Order order) {
          /*
             See: https://github.com/project-serum/serum-ts/blob/e51e3d9af0ab7026155b76a1824cea6507fc7ef7/packages/serum/src/instructions.js#L118
           */
@@ -20,9 +27,21 @@ public class SerumProgram extends Program {
           { pubkey: TOKEN_PROGRAM_ID, isSigner: false, isWritable: false },
           { pubkey: SYSVAR_RENT_PUBKEY, isSigner: false, isWritable: false },
         ];
-         */
+        */
+        final AccountMeta marketKey = new AccountMeta(market.getOwnAddress(), false, true);
+        //final AccountMeta openOrders = new AccountMeta(null, false, true)
+        final AccountMeta requestQueue = new AccountMeta(market.getRequestQueue(), false, true);
+        final AccountMeta payer = new AccountMeta(market.getRequestQueue(), false, true);
+        final AccountMeta owner = new AccountMeta(market.getRequestQueue(), true, false);
 
-        return createTransactionInstruction(null, null, null);
+
+        final List<AccountMeta> keys = new ArrayList<>();
+        keys.add(marketKey);
+        keys.add(requestQueue);
+        keys.add(payer);
+        keys.add(owner);
+
+        return createTransactionInstruction(null, keys, null);
     }
 
 }
