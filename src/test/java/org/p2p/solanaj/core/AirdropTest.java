ef36190d30c4e0c9714d2661448a68ed1fa30ae5
skynetcapital <67430414+skynetcapital@users.noreply.github.com>
2021-05-02 13:02:48 -0700

    Implement AirdropTest

--- src/test/java/org/p2p/solanaj/core/AirdropTest.java
+++ src/test/java/org/p2p/solanaj/core/AirdropTest.java
@@ -1,10 +1,9 @@
 package org.p2p.solanaj.core;
 
 import org.junit.Test;
-import org.p2p.solanaj.rpc.Cluster;
-import org.p2p.solanaj.rpc.RpcClient;
 import org.p2p.solanaj.token.TokenManager;
 
+import java.util.List;
 import java.util.logging.Logger;
 
 import static org.junit.Assert.assertTrue;
@@ -14,20 +13,27 @@ import static org.junit.Assert.assertTrue;
  * This illustrates the ability to Airdrop a given token directly to SOl addresses.
  */
 public class AirdropTest extends AccountBasedTest {
-    private static final Logger LOGGER = Logger.getLogger(AirdropTest.class.getName());
-    private final RpcClient client = new RpcClient(Cluster.MAINNET);
-    private final PublicKey publicKey = new PublicKey("skynetDj29GH6o6bAqoixCpDuYtWqi1rm8ZNx1hB3vq");
-    public final TokenManager tokenManager = new TokenManager();
     private static final PublicKey USDC_TOKEN_MINT = new PublicKey("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v");
+    private static final Logger LOGGER = Logger.getLogger(AirdropTest.class.getName());
+
+    private final PublicKey publicKey = new PublicKey("skynetDj29GH6o6bAqoixCpDuYtWqi1rm8ZNx1hB3vq");
+    private final PublicKey source = new PublicKey("A71WvME6ZhR4SFG3Ara7zQK5qdRSB97jwTVmB3sr7XiN");
+    public final TokenManager tokenManager = new TokenManager();
+
+    // List of recipients - ETL a file into this
+    private final List<PublicKey> recipients = List.of(publicKey);
+
+    private static final long AIRDROP_AMOUNT = 100;
+    private static final byte AIRDROP_DECIMALS = 6;
 
     @Test
     public void airdropTest() {
-
-
-
+        // Send airdrop
+        recipients.forEach(recipient -> {
+            tokenManager.transferCheckedToSolAddress(testAccount, source, publicKey, USDC_TOKEN_MINT, AIRDROP_AMOUNT, AIRDROP_DECIMALS);
+            LOGGER.info("Airdropped tokens to " + recipient.toBase58());
+        });
 
         assertTrue(true);
     }
-
-
 }
