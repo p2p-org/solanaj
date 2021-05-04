2c9fa21fbb4d2d6101252d9c44c36b0a75a0402a
skynetcapital <67430414+skynetcapital@users.noreply.github.com>
2021-05-04 10:57:44 -0700

    Implement SHA256 hashing for NamingManager.createNameRegistry.

--- src/main/java/org/p2p/solanaj/naming/NamingManager.java
+++ src/main/java/org/p2p/solanaj/naming/NamingManager.java
@@ -1,5 +1,7 @@
 package org.p2p.solanaj.naming;
 
+import com.google.common.hash.Hashing;
+import org.bitcoinj.core.Sha256Hash;
 import org.p2p.solanaj.core.Account;
 import org.p2p.solanaj.core.PublicKey;
 import org.p2p.solanaj.core.Transaction;
@@ -8,14 +10,22 @@ import org.p2p.solanaj.programs.TokenProgram;
 import org.p2p.solanaj.rpc.Cluster;
 import org.p2p.solanaj.rpc.RpcClient;
 import org.p2p.solanaj.rpc.RpcException;
+import org.p2p.solanaj.utils.ByteUtils;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 import java.util.logging.Logger;
 
+@SuppressWarnings({"MalformedFormatString", "UnstableApiUsage"})
 public class NamingManager {
 
     private final RpcClient client = new RpcClient(Cluster.MAINNET);
     private static final Logger LOGGER = Logger.getLogger(NamingManager.class.getName());
     private static final long DATA_LENGTH = 1000L;
+    private static final String HASH_PREFIX = "SPL Name Service";
+    private static final PublicKey NAME_PROGRAM_ID = new PublicKey("Gh9eN9nDuS3ysmAkKf4QJ6yBzf3YNqsn6MD8Ms3TsXmA");
 
     /**
      * Creates a .sol domain name with the specified name and payer.
@@ -33,10 +43,32 @@ public class NamingManager {
                                      final PublicKey nameClass,
                                      final PublicKey parentName) {
 
+        String fullDomainName = HASH_PREFIX + name;
+
+        byte[] hashedName = getHashedName(fullDomainName);
+        LOGGER.info(String.format("Name = %s, Sha256 = %s", fullDomainName, ByteUtils.bytesToHex(hashedName)).toLowerCase());
+
         long minimumBalanceForRentExemption = getMinimumBalanceForRentExemption();
         LOGGER.info(String.format("minimumBalanceForRentExemption = %d", minimumBalanceForRentExemption));
 
         return true;
+    }
+
+    /**
+     * Sha-256 the prefix + name, into byte array
+     * @param input domain name
+     * @return byte array of sha256 hashed string
+     */
+    private byte[] getHashedName(String input) {
+        MessageDigest digest = null;
+        try {
+            digest = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        byte[] encodedHash = digest.digest(input.getBytes(StandardCharsets.UTF_8));
+
+        return encodedHash;
     }
 
     /**
--- src/main/java/org/p2p/solanaj/programs/SerumProgram.java
+++ src/main/java/org/p2p/solanaj/programs/SerumProgram.java
@@ -191,7 +191,7 @@ public class SerumProgram extends Program {
         SerumUtils.writeLimit(result);
 
         byte[] arrayResult = result.array();
-        System.out.println("newOrderv3 instruction hex = " + bytesToHex(arrayResult));
+        System.out.println("newOrderv3 instruction hex = " + ByteUtils.bytesToHex(arrayResult));
 
         return arrayResult;
     }
@@ -260,16 +260,6 @@ public class SerumProgram extends Program {
         }
 
         return new PublicKey(base58Pubkey);
-    }
-
-    public static String bytesToHex(byte[] bytes) {
-        char[] hexChars = new char[bytes.length * 2];
-        for (int j = 0; j < bytes.length; j++) {
-            int v = bytes[j] & 0xFF;
-            hexChars[j * 2] = ByteUtils.HEX_ARRAY[v >>> 4];
-            hexChars[j * 2 + 1] = ByteUtils.HEX_ARRAY[v & 0x0F];
-        }
-        return new String(hexChars);
     }
 
 }
