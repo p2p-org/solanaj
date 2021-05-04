251400e1f18d2f1f660a0baaa90e09bc0b8f71d5
b1acksun <tkachenko.igor88@gmail.com>
2021-05-04 19:52:34 +0300

    fix: cluster

--- src/main/java/org/p2p/solanaj/rpc/Cluster.java
+++ src/main/java/org/p2p/solanaj/rpc/Cluster.java
@@ -3,7 +3,7 @@ package org.p2p.solanaj.rpc;
 public enum Cluster {
     DEVNET("https://devnet.solana.com"),
     TESTNET("https://testnet.solana.com"),
-    MAINNET("http://api.mainnet-beta.solana.com");
+    MAINNET("https://api.mainnet-beta.solana.com");
 
     private String endpoint;
 
