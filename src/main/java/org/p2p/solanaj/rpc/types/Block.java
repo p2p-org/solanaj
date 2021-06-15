f69ce09c930c865c23555cdb134b7d9ad15d42a0
cryptogosu <82475023+cryptogosu@users.noreply.github.com>
2021-06-14 20:35:05 -0500

    feat: add getBlock
    Added getBlock in preparation for Solana 1.8

--- src/main/java/org/p2p/solanaj/rpc/types/Block.java
+++ src/main/java/org/p2p/solanaj/rpc/types/Block.java
@@ -1,4 +1,30 @@
 package org.p2p.solanaj.rpc.types;
 
+import com.squareup.moshi.Json;
+
+import java.util.List;
+
 public class Block {
+
+    @Json(name = "blockTime")
+    private int blockTime;
+
+    @Json(name = "blockHeight")
+    private String blockHeight;
+
+    @Json(name = "blockhash")
+    private String blockhash;
+
+    @Json(name = "parentSlot")
+    private int parentSlot;
+
+    @Json(name = "previousBlockhash")
+    private String previousBlockhash;
+
+    @Json(name = "transactions")
+    private List<ConfirmedTransaction> transactions;
+
+    @Json(name = "rewards")
+    private List<Reward> rewards;
+
 }
